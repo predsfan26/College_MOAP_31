5ca4fc364e2e7eb98d79773c08b48b4882c3710e
David Matthews <predsfan26@gmail.com>
2011-09-05 08:33:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollegeMoap31::Application.config.secret_token = '3d56f534e74dfd0d49e8ceb67c54560ba27aa4e0aba10da294a3aebebccd06444f0b2929870e7afdfbdf5e686a5675d1e74f3751cafa6db5543a7b70e4c37d87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CollegeMoap31::Application.config.session_store :cookie_store, :key => '_college_moap_31_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CollegeMoap31::Application.config.session_store :active_record_store
